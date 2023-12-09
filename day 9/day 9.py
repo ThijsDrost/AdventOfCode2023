bd3ebadb4560da3a766be3965506f73a34d13383
ThijsDrost <120206360+ThijsDrost@users.noreply.github.com>
2023-12-09 10:20:02 +0100

    shortened

--- day 9/day 9.py
+++ day 9/day 9.py
@@ -1,18 +1,11 @@
-def difference(values):
+def difference(values, operator):
     differences = [y-x for x, y in zip(values, values[1:])]
     if len(set(differences)) == 1:
-        return values[-1] + differences[-1]
+        return operator(values, differences[0])
     else:
-        return values[-1] + difference(differences)
-
-def difference_back(values):
-    differences = [y-x for x, y in zip(values, values[1:])]
-    if len(set(differences)) == 1:
-        return values[0] - differences[0]
-    else:
-        return values[0] - difference_back(differences)
+        return operator(values, difference(differences, operator))
 
 
 lines = [list(map(int, line.split())) for line in open('input.txt').read().split('\n') if line]
-print(f'part 1: {sum(list(map(difference, lines)))}')
-print(f'part 2: {sum(list(map(difference_back, lines)))}')
+print(f'part 1: {sum(list(map(lambda x: difference(x, lambda y, z: y[-1] + z), lines)))}')
+print(f'part 12 {sum(list(map(lambda x: difference(x, lambda y, z: y[0] - z), lines)))}')
