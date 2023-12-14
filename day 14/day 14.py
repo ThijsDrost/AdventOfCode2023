cd343560c688d53359eb7857411820b52a34c62e
Thijs <drosttj@xs4all.nl>
2023-12-14 16:05:02 +0100

    day 14: second (sadly slower) algorithm

--- day 14/day 14.py
+++ day 14/day 14.py
@@ -1,3 +1,6 @@
+from time import time
+
+
 def step(r_boulders, s_boulders, f_size, direction):
     new_r_boulders = set()
     for i in range(f_size):
@@ -30,6 +33,20 @@ def step(r_boulders, s_boulders, f_size, direction):
     return new_r_boulders
 
 
+def step2(r_boulders, s_boulders, f_size, direction):
+    change = {'n': (-1, 0), 's': (1, 0), 'w': (0, -1), 'e': (0, 1)}[direction]
+    sorting = {'n': (1, f_size), 's': (-1, f_size), 'w': (f_size, 1), 'e': (f_size, -1)}[direction]
+    r_boulders = sorted(r_boulders, key=(lambda x: (x[0] * sorting[0] + x[1] * sorting[1])))
+    for index, coord in enumerate(r_boulders):
+        new_coord = (coord[0] + change[0], coord[1] + change[1])
+        while ((0 <= new_coord[0] < f_size) and (0 <= new_coord[1] < f_size)
+               and (new_coord not in s_boulders) and (new_coord != r_boulders[index - 1])):
+            coord = new_coord
+            new_coord = (coord[0] + change[0], coord[1] + change[1])
+        r_boulders[index] = coord
+    return r_boulders
+
+
 def new_round(r_boulders, s_boulders, f_size):
     r_boulders, s_boulders = set(r_boulders), set(s_boulders)
     r_boulders = step(r_boulders, s_boulders, f_size, 'n')
@@ -38,12 +55,33 @@ def new_round(r_boulders, s_boulders, f_size):
     r_boulders = step(r_boulders, s_boulders, f_size, 'e')
     return r_boulders
 
+def new_round2(r_boulders, s_boulders, f_size):
+    r_boulders, s_boulders = list(r_boulders), set(s_boulders)
+    r_boulders = step2(r_boulders, s_boulders, f_size, 'n')
+    r_boulders = step2(r_boulders, s_boulders, f_size, 'w')
+    r_boulders = step2(r_boulders, s_boulders, f_size, 's')
+    r_boulders = step2(r_boulders, s_boulders, f_size, 'e')
+    return r_boulders
+
 
 def weight(r_boulders, f_size):
     total = 0
     for (i, j) in r_boulders:
         total += f_size-i
     return total
+
+def print_new(r_boulders, s_boulders, f_size):
+    for i in range(f_size):
+        print(' ', end='')
+        for j in range(f_size):
+            if (i, j) in r_boulders:
+                print('O', end='')
+            elif (i, j) in s_boulders:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+    print()
 
 
 lines = [list(line) for line in open('input.txt', 'r').read().split('\n') if line]
@@ -70,6 +108,7 @@ for i in range(len(lines)):
 f_size = len(lines)
 assert f_size == len(lines[1])
 
+start = time()
 prev = {}
 for i in range(1, 1_000_000_001):
     r_boulders = tuple(new_round(tuple(r_boulders), tuple(s_boulders), f_size))
@@ -82,3 +121,29 @@ for i in range(1, 1_000_000_001):
         print(f"Part 2: {weight(r_boulders, f_size)}")
         break
     prev[hash_val] = i
+print(f"Time: {time()-start}")
+
+r_boulders = set()
+s_boulders = set()
+for i in range(len(lines)):
+    for j in range(len(lines[0])):
+        if lines[i][j] == 'O':
+            r_boulders.add((i, j))
+        elif lines[i][j] == '#':
+            s_boulders.add((i, j))
+
+start = time()
+prev = {}
+r_boulders = sorted(r_boulders, key=(lambda x: (x[0] + x[1] * f_size)))
+for i in range(1, 1_000_000_001):
+    r_boulders = tuple(new_round2(r_boulders, s_boulders, f_size))
+    hash_val = hash(r_boulders)
+    if hash_val in prev.keys():
+        last = prev[hash_val]
+        togo = (1_000_000_000 - i) % (i-last)
+        for i in range(togo):
+            r_boulders = tuple(new_round2(r_boulders, s_boulders, f_size))
+        print(f"Part 2: {weight(r_boulders, f_size)}")
+        break
+    prev[hash_val] = i
+print(f"Time: {time()-start}")
